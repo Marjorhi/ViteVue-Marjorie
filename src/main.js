dcdcfa6fc33515cdd4984b48432c12f0d305bb3a
Marjorhi <marjoriejetajobe0524@gmail.com>
2023-09-20 20:12:58 +0800

    Importing index.umd.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/js/index.umd.js'
 import App from './App.vue'
 
 createApp(App).mount('#app')
