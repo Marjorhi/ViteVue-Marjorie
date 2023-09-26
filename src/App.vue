a458aaa7223c291e473455217a6c5c5a06c0336a
Marjorhi <marjoriejetajobe0524@gmail.com>
2023-09-26 21:30:46 +0800

    Form for first name

--- src/App.vue
+++ src/App.vue
@@ -1,2 +1,20 @@
-<template> </template>
+<script setup>
+import { ref } from 'vue';
 
+
+const text = ref('');
+const submittedName = ref('');
+
+const handleSubmit = () => {
+  submittedName.value = text.value;
+};
+</script>
+
+<template>
+  <p>First Name</p>
+  <input v-model="text">
+  <button @click="handleSubmit">Submit</button>
+  
+  <!-- Display the submitted name -->
+  <p> {{ submittedName }}</p>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import { createApp } from 'vue'
-import './style.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/js/index.umd.js'
 import App from './App.vue'
