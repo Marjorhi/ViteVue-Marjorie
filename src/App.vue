d4fdb33a2baed2e427b3b708f21c5b28c8327f54
Marjorhi <marjoriejetajobe0524@gmail.com>
2023-09-26 21:42:04 +0800

    Adding birthday

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,15 @@ import { ref } from 'vue';
 
 const firstName = ref('');
 const lastName = ref('');
+const birthday = ref('');
 const submittedFirstName = ref('');
 const submittedLastName = ref('');
+const submittedBirthday = ref('');
 
 const handleSubmit = () => {
   submittedFirstName.value = firstName.value;
   submittedLastName.value = lastName.value;
+  submittedBirthday.value = birthday.value;
 };
 </script>
 
@@ -21,10 +24,13 @@ const handleSubmit = () => {
     <p>Last Name</p>
     <input v-model="lastName">
   </div>
+  <div>
+    <p>Birthday</p>
+    <input type="date" v-model="birthday">
+  </div>
   <button @click="handleSubmit">Submit</button>
-
-  
 
   <!-- Display the submitted names -->
   <p>{{ submittedFirstName }} {{ submittedLastName }}</p>
+  <p>{{ submittedBirthday }}</p>
 </template>
