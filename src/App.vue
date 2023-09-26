9771b18b7c7791dba6bff98d7b2ee121ae1b850c
Marjorhi <marjoriejetajobe0524@gmail.com>
2023-09-26 22:04:58 +0800

    Adding gender

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ import { ref } from 'vue';
 const firstName = ref('');
 const lastName = ref('');
 const birthday = ref('');
+const selected = ref(''); // Added selected as a ref variable for gender
 const submittedFirstName = ref('');
 const submittedLastName = ref('');
 const submittedBirthday = ref('');
@@ -28,9 +29,22 @@ const handleSubmit = () => {
     <p>Birthday</p>
     <input type="date" v-model="birthday">
   </div>
+  <div>
+    <p>Gender</p>
+    <select v-model="selected">
+      <option disabled value="">Please select one</option>
+      <option value="female">FEMALE</option>
+      <option value="male">MALE</option>
+      <option value="other">OTHER</option>
+    </select>
+    <p v-if="selected === 'female'"></p>
+    <p v-else-if="selected === 'male'"></p>
+    <p v-else></p>
+  </div>
   <button @click="handleSubmit">Submit</button>
 
-  <!-- Display the submitted names -->
+  <!-- Display the submitted information -->
   <p>{{ submittedFirstName }} {{ submittedLastName }}</p>
   <p>{{ submittedBirthday }}</p>
+  <p>{{ selected }}</p>
 </template>
