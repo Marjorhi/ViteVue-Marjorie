6bbd2cfe0ff7e668e0b010ac95a934a6fbb98bbf
Marjorhi <marjoriejetajobe0524@gmail.com>
2023-11-25 13:25:14 +0800

    Importing the basecondition and baseloop to app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,22 @@
-<template>
+<script>
+import BaseCondition from './components/BaseCondition.vue';
+import BaseLoop from './components/BaseLoop.vue';
 
+export default {
+    components: {
+        BaseCondition,
+        BaseLoop
+    },
+};
+</script>
+
+
+<template>
+<div id="app">
+    <BaseCondition/>
+</div>
+
+<div id="app">
+    <BaseLoop/>
+</div>
 </template>
